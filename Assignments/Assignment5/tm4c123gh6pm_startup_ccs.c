2d2b59f9a6ebc32a90e40c67010d52ecf29ccfd7
Zibrandtsen <jrz@live.dk>
2024-03-25 13:16:23 +0100

    Ready for assignment5

--- Assignments/Assignment5/tm4c123gh6pm_startup_ccs.c
+++ Assignments/Assignment5/tm4c123gh6pm_startup_ccs.c
@@ -24,6 +24,8 @@
 
 #include <stdint.h>
 
+
+void systick_handler(void);
 //*****************************************************************************
 //
 // Forward declaration of the default fault handlers.
@@ -82,7 +84,7 @@ void (* const g_pfnVectors[])(void) =
     IntDefaultHandler,                      // Debug monitor handler
     0,                                      // Reserved
     IntDefaultHandler,                      // The PendSV handler
-    IntDefaultHandler,                      // The SysTick handler
+    systick_handler,                      // The SysTick handler
     IntDefaultHandler,                      // GPIO Port A
     IntDefaultHandler,                      // GPIO Port B
     IntDefaultHandler,                      // GPIO Port C
